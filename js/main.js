942451cc110f600096e2f9b509707a9137f9e078
Justin Zelinsky <jpzelinsky@gmail.com>
2015-06-18 10:35:19 -0400

    moved files around

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+  var decToHex = function(n) {
+    return parseInt(n, 10).toString(16);
+  };
+
+  var hexToDec = function(n) {
+    return parseInt(n, 16).toString(10);
+  };
+
+  var padToSix = function(hexString) {
+    while (hexString.length % 6 !== 0) {
+      hexString += "0";
+    }
+    return hexString;
+  };
+
+  $("#inputStr").keyup(function() {
+    var inputStr = $("#inputStr").val().toLowerCase();
+    var hexString;
+    if (inputStr === "") {
+      hexString = "000000";
+    } else {
+      var inputStrArr = inputStr.split("");
+      hexString = padToSix(_.map(inputStrArr, function(char) {
+        return decToHex(char.charCodeAt());
+      }).join(""));
+
+      while (hexString.length !== 6) {
+        var hexStringArr = hexString.match(/.{2}/g);
+        hexString = padToSix(_.map(hexStringArr, function(doubleChar) {
+          var doubleCharSplit = doubleChar.split("");
+          var sum = parseInt(hexToDec(doubleCharSplit[0])) + parseInt(hexToDec(doubleCharSplit[1]));
+          return decToHex(sum);
+        }).join(""));
+      }
+    }
+
+    $(".hex").html("#" + hexString);
+    $("body").css("background-color", "#" + hexString);
+  });
+});
