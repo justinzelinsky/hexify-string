7be362c1a5291d6e8c4e4d5c7893f0ff16fefe31
Justin Zelinsky <jpzelinsky@gmail.com>
2018-12-28 09:26:20 -0500

    better

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+export function stringToHex(str) {
+  let hexStr = 'FFFFFF';
+  if (str) {
+    hexStr = str
+      .split('')
+      .map(c => c.charCodeAt().toString(16))
+      .join('')
+      .padEnd(6, '0');
+    while (hexStr.length !== 6) {
+        hexStr = hexStr
+          .match(/.{2}/g)
+          .map(pair => pair
+            .split('')
+            .reduce((first, second) => 
+              parseInt(first, 16) + parseInt(second, 16)
+            )
+            .toString(16)
+        );
+        hexStr = hexStr
+          .join('')
+          .padEnd(6, '0');
+    }
+  }
+  return hexStr;
+}
